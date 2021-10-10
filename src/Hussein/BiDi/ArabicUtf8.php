799d704e39016c67460bcbbd6ecb8030d2db0342
H. Elturkey <13208801+helturkey@users.noreply.github.com>
2021-10-10 21:09:55 +0200

    Add files via upload

---
+++ src/Hussein/BiDi/ArabicUtf8.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Hussein\BiDi;
+
+require 'Lib' . DIRECTORY_SEPARATOR . 'Bidi.php';
+
+
+class ArabicUtf8
+{
+
+    static public function convert(array|string $text, $forcertl = false)
+    {
+        $bidi = new \Bidi();
+
+        if (\is_array($text)) {
+            $str = array();
+            foreach ($text as $line) {
+                $chars = $bidi->utf8Bidi($bidi->UTF8StringToArray($line), $forcertl);
+                $line = '';
+                foreach ($chars as $char) {
+                    $line .= $bidi->unichr($char);
+                }
+
+                $str[] = $line;
+            }
+            return $str;
+        } elseif (\is_string($text)) {
+            $chars = $bidi->utf8Bidi($bidi->UTF8StringToArray($text), $forcertl);
+            $line = '';
+            foreach ($chars as $char) {
+                $line .= $bidi->unichr($char);
+            }
+
+            return $line;
+        }
+    }
+}
