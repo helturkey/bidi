270fe066d7d93e341c8963ec6fafd93c459054ce
H. Elturkey <13208801+helturkey@users.noreply.github.com>
2021-10-12 03:23:02 +0200

    improvements
    improvements

--- src/Bidirection/ArabicUtf8.php
+++ src/Bidirection/ArabicUtf8.php
@@ -8,7 +8,7 @@ require_once 'Lib' . DIRECTORY_SEPARATOR . 'Bidi.class.php';
 class ArabicUtf8
 {
 
-    static public function convert(array|string $text, $forcertl = false): array|string
+    public static function convert(array|string $text, $forcertl = false): array|string
     {
         $bidi = new \Bidi();
 
@@ -33,5 +33,77 @@ class ArabicUtf8
 
             return $line;
         }
+    }
+
+    public static function mb_wordwrap(string $string, int $width = 75, $break = "\n", bool $cut = false): string
+    {
+        $string = (string)$string;
+        if ($string === '') {
+            return '';
+        }
+
+        $break = (string)$break;
+        if ($break === '') {
+            trigger_error('Break string cannot be empty', E_USER_ERROR);
+        }
+
+        $width = (int)$width;
+        if ($width === 0 && $cut) {
+            trigger_error('Cannot force cut when width is zero', E_USER_ERROR);
+        }
+
+        if (strlen($string) === mb_strlen($string)) {
+            return wordwrap($string, $width, $break, $cut);
+        }
+
+        $stringWidth = mb_strlen($string);
+        $breakWidth = mb_strlen($break);
+
+        $result = '';
+        $lastStart = $lastSpace = 0;
+
+        for ($current = 0; $current < $stringWidth; $current++) {
+            $char = mb_substr($string, $current, 1);
+
+            $possibleBreak = $char;
+            if ($breakWidth !== 1) {
+                $possibleBreak = mb_substr($string, $current, $breakWidth);
+            }
+
+            if ($possibleBreak === $break) {
+                $result .= mb_substr($string, $lastStart, $current - $lastStart + $breakWidth);
+                $current += $breakWidth - 1;
+                $lastStart = $lastSpace = $current + 1;
+                continue;
+            }
+
+            if ($char === ' ') {
+                if ($current - $lastStart >= $width) {
+                    $result .= mb_substr($string, $lastStart, $current - $lastStart) . $break;
+                    $lastStart = $current + 1;
+                }
+
+                $lastSpace = $current;
+                continue;
+            }
+
+            if ($current - $lastStart >= $width && $cut && $lastStart >= $lastSpace) {
+                $result .= mb_substr($string, $lastStart, $current - $lastStart) . $break;
+                $lastStart = $lastSpace = $current;
+                continue;
+            }
+
+            if ($current - $lastStart >= $width && $lastStart < $lastSpace) {
+                $result .= mb_substr($string, $lastStart, $lastSpace - $lastStart) . $break;
+                $lastStart = $lastSpace = $lastSpace + 1;
+                continue;
+            }
+        }
+
+        if ($lastStart !== $current) {
+            $result .= mb_substr($string, $lastStart, $current - $lastStart);
+        }
+
+        return $result;
     }
 }
